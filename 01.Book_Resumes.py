d43380dd565064e8133fd98fa80c09eca7f2c694
Anton Gavrailov <agavrailov@bianor.com>
2023-02-20 22:55:56 +0200

    Chapters enumerated by names. Bullet style prompt

--- 01.Book_Resumes.py
+++ 01.Book_Resumes.py
@@ -6,15 +6,53 @@ openai.api_key = "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"
 
 # Define book title and chapter names
 book_title = "Thinking, Fast and Slow"
-chapter_names = [f"Chapter {i}" for i in range(1, 40)]
-
+chapter_names = [
+    "The Characters of the Story",
+    "Attention and Effort",
+    "The Lazy Controller",
+    "The Associative Machine",
+    "Cognitive Ease",
+    "Norms, Surprises, and Causes",
+    "A Machine for Jumping to Conclusions",
+    "How Judgments Happen",
+    "Answering an Easier Question",
+    "The Law of Small Numbers",
+    "Anchors",
+    "The Science of Availability",
+    "Availability, Emotion, and Risk",
+    "Tom W’s Specialty",
+    "Linda: Less is More",
+    "Causes Trump Statistics",
+    "Regression to the Mean",
+    "Taming Intuitive Predictions",
+    "The Illusion of Understanding",
+    "The Illusion of Validity",
+    "Intuitions Vs. Formulas",
+    "Expert Intuition: When Can We Trust It?",
+    "The Outside View",
+    "The Engine of Capitalism",
+    "Bernoulli’s Errors",
+    "Prospect Theory",
+    "The Endowment Effect",
+    "Bad Events",
+    "The Fourfold Pattern",
+    "Rare Events",
+    "Risk Policies",
+    "Keeping Score",
+    "Reversals",
+    "Frames and Reality",
+    "Two Selves",
+    "Life as a Story",
+    "Experienced Well-Being",
+    "Thinking About Life"
+]
 
 # Loop through each chapter and summarize using OpenAI API
 for chapter_name in chapter_names:
     # Build prompt string
-    prompt = f"Summarize {chapter_name} of {book_title} by Daniel Kahneman in 400-600 tokens, highlighting the most important points. Start with the name of the chapter, don't mention the book. Speak as you are the author, but do not use first person. Tell it as story."
+    prompt = f"Summarize '{chapter_name}' of '{book_title}' by Daniel Kahneman 'Thinking, Fast and Slow', using bullet points.\nPlease, refrain from expressing any personal opinion\nAt the end, provide conclusion of the chapter.\n\n"
 
-# Query OpenAI API
+    # Query OpenAI API
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt= prompt,
